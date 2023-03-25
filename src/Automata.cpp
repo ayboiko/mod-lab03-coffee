b4e5e7eace54cc5661d3bffde360b08f1208d21e
ayboiko <126852383+ayboiko@users.noreply.github.com>
2023-03-25 16:27:18 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,63 +1,64 @@
 #include "Automata.h"
 #include <iostream>
 #include <string>
-CofeeMachine :: CofeeMachine(int* prices_, std :: string* menu_, int kol_){
-  st= OFF , indchoose= -1 , cash = 0;
+CofeeMachine :: CofeeMachine(int* prices_, std :: string* menu_, int kol_) {
+  st = OFF , indchoose = -1 , cash = 0;
   prices = prices_, menu = menu_, kol = kol_;
 }
-void CofeeMachine :: on(){
-  if(st == OFF){
+void CofeeMachine :: on() {
+  if (st == OFF) {
     st = WAIT;
   }
 }
-void CofeeMachine :: off(){
-  if(st == WAIT){
+void CofeeMachine :: off() {
+  if (st == WAIT) {
     st = OFF;
   }
 }
-void CofeeMachine :: coin(int a){
-  if(st == WAIT || st == ACCEPT){
+void CofeeMachine :: coin(int a) {
+  if (st == WAIT || st == ACCEPT) {
     st = ACCEPT;
     cash = cash + a;
   }
 }
-void CofeeMachine :: cancel(){
-  if(st == ACCEPT || st == CHECK){
+void CofeeMachine :: cancel() {
+  if (st == ACCEPT || st == CHECK) {
     st = WAIT;
     indchoose = -1;
   }
 }
-void CofeeMachine :: finish(){
-  if(st == COOK){
+void CofeeMachine :: finish() {
+  if (st == COOK) {
     st = WAIT;
     indchoose = -1;
     std :: cout<< "Take your drink \n";
   }
 }
-void CofeeMachine :: cook(){
-  if(st == ACCEPT){
+void CofeeMachine :: cook() {
+  if (st == ACCEPT){
     st = COOK;
   }
 }
-void CofeeMachine :: choice(int ind){
-  if(st == ACCEPT){
-    if( ind >= 1 && ind <= kol ){
+void CofeeMachine :: choice(int ind) {
+  if (st == ACCEPT) {
+    if ( ind >= 1 && ind <= kol ) {
     indchoose = ind - 1; st = CHECK;
     }
-    else{
+    else
+    {
       indchoose = -1;
     }
   }
 }
-void CofeeMachine :: check(){
-  if(st == CHECK){
-    if( cash >= prices[indchoose] && indchoose != -1){
-      cash=cash-prices[indchoose];
+void CofeeMachine :: check() {
+  if (st == CHECK) {
+    if ( cash >= prices[indchoose] && indchoose != -1) {
+      cash = cash - prices[indchoose];
     }
   }
 }
-std :: string CofeeMachine :: getState(){
-  switch(st){
+std :: string CofeeMachine :: getState() {
+  switch (st) {
     case OFF : return "OFF";
     case WAIT : return "WAIT";
     case ACCEPT : return "ACCEPT";
