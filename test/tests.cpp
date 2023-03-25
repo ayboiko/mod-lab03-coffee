31dded33f470ac3c97a53b1d27ac3e25fc3ed7e6
ayboiko <126852383+ayboiko@users.noreply.github.com>
2023-03-25 17:28:10 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -28,7 +28,6 @@ TEST(task1, test2) {
   std :: string result = cof.getState();
   cof.cook();
   cof.finish();
-  std :: string result = cof.getState();
   EXPECT_EQ("CHECK", result);
 }
 TEST(task1, test3) {
